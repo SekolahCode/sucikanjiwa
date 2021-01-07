2bec66d6e703b10aa60386f085a3fb4bc47af9ea
Shukri Yusof <shukriyusof65@gmail.com>
2021-01-07 23:01:58 +0800

    adding google analytics to the right place

--- resources/views/website/layouts/header.blade.php
+++ resources/views/website/layouts/header.blade.php
@@ -4,6 +4,16 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, shrink-to-fit=no">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-PSXS54Y0M6"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-PSXS54Y0M6');
+    </script>
+    <!-- End of Global site tag (gtag.js) - Google Analytics -->
     <title>Landing Page - Sucikan Jiwa</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('custom/favicon_io/favicon.ico') }}"/>
     <!-- BEGIN GLOBAL MANDATORY STYLES -->
@@ -18,5 +28,5 @@
     <!-- BEGIN PAGE LEVEL PLUGINS/CUSTOM STYLES -->
     <link href="{{ asset('assets/css/pages/faq/faq.css') }}" rel="stylesheet" type="text/css" />
     <link href="{{ asset('cork/css/components/timeline/custom-timeline.css') }}" rel="stylesheet" type="text/css" />
-    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->     
+    <!-- END PAGE LEVEL PLUGINS/CUSTOM STYLES -->
 </head>
